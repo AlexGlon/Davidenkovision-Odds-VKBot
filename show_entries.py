69f15bd5413cd1f9526eff999bb0d58e3c1e8894
Alexei Glon <alexglon@outlook.com>
2021-06-01 13:03:27 +0300

    Coefficient output logic expanded

--- show_entries.py
+++ show_entries.py
@@ -6,6 +6,7 @@ with open('entries.json', 'r') as file:
     entries = json.load(file)
 
 
+# TODO: rewrite this function
 def entry_iter(bet=False):
     total_output = ''
     for entry in entries:
@@ -17,14 +18,21 @@ def entry_iter(bet=False):
         if bet:
             with open('stats.json', 'r') as stats_f:
                 stats = json.load(stats_f)
-            output_entryname = f" | {entry['artist']} — {entry['entry']} | {stats['entry_stats'][entry['running_order']]['current_coef']}"
+            entries[entries.index(entry)]['coefficient'] = stats['entry_stats'][entry['running_order']]['current_coef']
 
         output = output + output_year + output_entryname
-        # print(entry["running_order"], flags.country_dict[entry['country']],
-        #       entry["country"], f"{(lambda x: '' if x == None else x)(entry['year'])}",
-        #       entry["artist"], '-', entry["entry"])
-
         total_output = total_output + output + '\n'
+
+    if bet:
+        bets = sorted(entries, key=lambda i: i['coefficient'])
+        total_output = ''
+        for entry in bets:
+            output_year = (lambda x: ' ' if x == None else f" {x} ")(entry['year'])
+            output = f"{bets.index(entry)+1}. {flags.country_dict[entry['country']]} {entry['country']}"
+            output_entryname = f" | {entry['artist']} — {entry['entry']} | {entry['coefficient']}"
+
+            output = output + output_year + output_entryname
+            total_output = total_output + output + '\n'
 
     print(total_output)
     return total_output
