a1a2605df05a7eb68be3558c2ace63ba593f1ed7
Alexei Glon <kot1alexx@gmail.com>
2022-07-17 21:08:54 +0300

    Added new flags to `flags.py`

--- flags.py
+++ flags.py
@@ -2,57 +2,96 @@ import emoji
 
 # COUNTRY = f"{emoji.emojize(':COUNTRY:')}"
 
+Albania = f"{emoji.emojize(':Albania:')}"
+Armenia = f"{emoji.emojize(':Armenia:')}"
 Australia = f"{emoji.emojize(':Australia:')}"
+Austria = f"{emoji.emojize(':Austria:')}"
 Azerbaijan = f"{emoji.emojize(':Azerbaijan:')}"
 # Žyvie Viečna!
 Belarus = f"{emoji.emojize(':white_circle::red_circle::white_circle:')}"
 Belgium = f"{emoji.emojize(':Belgium:')}"
+BosniaHerzegovina = f"{emoji.emojize(':Bosnia_&_Herzegovina:')}"
 Cyprus = f"{emoji.emojize(':Cyprus:')}"
 Czechia = f"{emoji.emojize(':Czechia:')}"
+Denmark = f"{emoji.emojize(':Denmark:')}"
+Estonia = f"{emoji.emojize(':Estonia:')}"
+Finland = f"{emoji.emojize(':Finland:')}"
 France = f"{emoji.emojize(':France:')}"
 Georgia = f"{emoji.emojize(':Georgia:')}"
 Germany = f"{emoji.emojize(':Germany:')}"
 Greece = f"{emoji.emojize(':Greece:')}"
+Hungary = f"{emoji.emojize(':Hungary:')}"
+Iceland = f"{emoji.emojize(':Iceland:')}"
+Ireland = f"{emoji.emojize(':Ireland:')}"
 Israel = f"{emoji.emojize(':Israel:')}"
 Italy = f"{emoji.emojize(':Italy:')}"
 Latvia = f"{emoji.emojize(':Latvia:')}"
 Lithuania = f"{emoji.emojize(':Lithuania:')}"
+Luxembourg = f"{emoji.emojize(':Luxembourg:')}"
+Macedonia = f"{emoji.emojize(':North_Macedonia:')}"
 Malta = f"{emoji.emojize(':Malta:')}"
 Moldova = f"{emoji.emojize(':Moldova:')}"
 Morocco = f"{emoji.emojize(':Morocco:')}"
+Netherlands = f"{emoji.emojize(':Netherlands:')}"
 Norway = f"{emoji.emojize(':Norway:')}"
+Poland = f"{emoji.emojize(':Poland:')}"
+Portugal = f"{emoji.emojize(':Portugal:')}"
+Romania = f"{emoji.emojize(':Romania:')}"
 Russia = f"{emoji.emojize(':Russia:')}"
 SanMarino = f"{emoji.emojize(':San_Marino:')}"
 Serbia = f"{emoji.emojize(':Serbia:')}"
+Slovakia = f"{emoji.emojize(':Slovakia:')}"
+Slovenia = f"{emoji.emojize(':Slovenia:')}"
 Spain = f"{emoji.emojize(':Spain:')}"
 Sweden = f"{emoji.emojize(':Sweden:')}"
+Switzerland = f"{emoji.emojize(':Switzerland:')}"
 Ukraine = f"{emoji.emojize(':Ukraine:')}"
 UnitedKingdom = f"{emoji.emojize(':United_Kingdom:')}"
 
+# this dict uses country names claimed in `countries` DB table
 country_dict = {
+    "Albania": Albania,
+    "Armenia": Armenia,
     "Australia": Australia,
+    "Austria": Austria,
     "Azerbaijan": Azerbaijan,
     "Belarus": Belarus,
     "Belgium": Belgium,
+    "Bosnia and Herzegovina": BosniaHerzegovina,
     "Cyprus": Cyprus,
-    "Czech Republic": Czechia,
+    "Czechia": Czechia,
+    "Denmark": Denmark,
+    "Estonia": Estonia,
+    "Finland": Finland,
     "France": France,
     "Georgia": Georgia,
     "Germany": Germany,
     "Greece": Greece,
+    "Hungary": Hungary,
+    "Iceland": Iceland,
+    "Ireland": Ireland,
     "Israel": Israel,
     "Italy": Italy,
     "Latvia": Latvia,
     "Lithuania": Lithuania,
+    "Luxembourg": Luxembourg,
+    "Macedonia": Macedonia,
     "Malta": Malta,
     "Moldova": Moldova,
     "Morocco": Morocco,
+    "Netherlands": Netherlands,
     "Norway": Norway,
+    "Poland": Poland,
+    "Portugal": Portugal,
+    "Romania": Romania,
     "Russia": Russia,
     "San Marino": SanMarino,
     "Serbia": Serbia,
+    "Slovakia": Slovakia,
+    "Slovenia": Slovenia,
     "Spain": Spain,
     "Sweden": Sweden,
+    "Switzerland": Switzerland,
     "Ukraine": Ukraine,
     "United Kingdom": UnitedKingdom,
 }
