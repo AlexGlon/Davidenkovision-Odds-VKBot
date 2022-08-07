aeb122bdab044cd5b4a7116905c50df41ee61c4c
Alexei Glon <kot1alexx@gmail.com>
2022-08-07 22:00:47 +0300

    Small tweaks

--- core/dotenv_variables.py
+++ core/dotenv_variables.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 COEFFICIENT_OBSCURITY = os.environ.get('HIDE_COEFFICIENTS_UNTIL_CONTESTS_FINISHES')
-MINUTES_PER_BACKUP = os.environ.get('MINUTES_PER_BACKUP')
+MINUTES_PER_BACKUP = int(os.environ.get('MINUTES_PER_BACKUP'))
 TOKEN = os.environ.get('TOKEN')
